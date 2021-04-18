7d19b7b62883dd3c713cdf6e71ec21fff11d373d
Miguel García Soler <miguelgarciasoler0@gmail.com>
2021-04-18 19:49:03 +0200

    output can now be saved in a file

--- rptree/cli.py
+++ rptree/cli.py
@@ -15,7 +15,7 @@ def main():
     if not root_dir.is_dir():
         print("The specified root directory doesn't exist")
         sys.exit()
-    tree = DirectoryTree(root_dir)
+    tree = DirectoryTree(root_dir, dir_only=args.dir_only, output_file=args.output_file)
     tree.generate()
 
 def parse_cmd_line_arguments() -> argparse.ArgumentParser:
@@ -32,5 +32,19 @@ def parse_cmd_line_arguments() -> argparse.ArgumentParser:
         nargs="?",
         default=".",
         help="Generate a full directory tree starting at ROOT_DIR",
+    )
+    parser.add_argument(
+        "-d",
+        "--dir-only",
+        action="store_true",
+        help="Generate a directory-only tree",
+    )
+    parser.add_argument(
+        "-o",
+        "--output-file",
+        metavar="OUTPUT_FILE",
+        nargs="?",
+        default=sys.stdout,
+        help="Generate a full directory tree and save it to a file",
     )
     return parser.parse_args()
--- rptree/rptree.py
+++ rptree/rptree.py
@@ -3,6 +3,7 @@
 """This module provides RP Tree main module."""
 
 import os
+import sys
 import pathlib
 
 PIPE = "│"
@@ -13,18 +14,26 @@ SPACE_PREFIX = "    "
 
 
 class DirectoryTree:
-    def __init__(self, root_dir):
-        self._generator = _TreeGenerator(root_dir)
+    def __init__(self, root_dir, dir_only=False, output_file=sys.stdout):
+        self._output_file = output_file
+        self._generator = _TreeGenerator(root_dir, dir_only)
 
     def generate(self):
         tree = self._generator.build_tree()
-        for entry in tree:
-            print(entry)
+        if self._output_file != sys.stdout:
+            # Wrap the tree in a markdown code block
+            tree.insert(0, "```")
+            tree.append("```")
+            self._output_file = open(self._output_file, mode="w", encoding="UTF-8")
+        with self._output_file as stream:
+            for entry in tree:
+                print(entry, file=stream)
 
 
 class _TreeGenerator:
-    def __init__(self, root_dir):
+    def __init__(self, root_dir, dir_only=False):
         self._root_dir = pathlib.Path(root_dir)
+        self._dir_only = dir_only
         self._tree = []
 
     def build_tree(self):
@@ -37,8 +46,7 @@ class _TreeGenerator:
         self._tree.append(PIPE)
 
     def _tree_body(self, directory, prefix=""):
-        entries = directory.iterdir()
-        entries = sorted(entries, key=lambda entry: entry.is_file())
+        entries = self.prepare_entries(directory)
         entries_count = len(entries)
         for index, entry in enumerate(entries):
             connector = ELBOW if (index == entries_count - 1) else TEE
@@ -47,6 +55,14 @@ class _TreeGenerator:
                     entry, index, entries_count, prefix, connector)
             else:
                 self._add_file(entry, prefix, connector)
+
+    def prepare_entries(self, directory):
+        entries = directory.iterdir()
+        if self._dir_only:
+            entries = [entry for entry in entries if entry.is_dir()]
+            return entries
+        entries = sorted(entries, key=lambda entry: entry.is_file())
+        return entries
 
     def _add_directory(self, directory, index, entries_count, prefix, connector):
         self._tree.append(f"{prefix}{connector} {directory.name}{os.sep}")
